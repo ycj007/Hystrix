2a89c11d51c20a0fbc2a6de5142e57492a63c7e5
chongjie.yuan <chongjie.yuan@mtime.com>
2018-11-13 14:35:25 +0800

    update code

--- hystrix-core/src/main/java/com/netflix/hystrix/AbstractCommand.java
+++ hystrix-core/src/main/java/com/netflix/hystrix/AbstractCommand.java
@@ -380,32 +380,28 @@ import java.util.concurrent.atomic.AtomicReference;
 
         //mark the command as CANCELLED and store the latency (in addition to standard cleanup)
         final Action0 unsubscribeCommandCleanup = new Action0() {
+
+
+            private void markTerminalEventData(){
+                if (!_cmd.executionResult.containsTerminalEvent()) {
+                    _cmd.eventNotifier.markEvent(HystrixEventType.CANCELLED, _cmd.commandKey);
+                    try {
+                        executionHook.onUnsubscribe(_cmd);
+                    } catch (Throwable hookEx) {
+                        logger.warn("Error calling HystrixCommandExecutionHook.onUnsubscribe", hookEx);
+                    }
+                    _cmd.executionResultAtTimeOfCancellation = _cmd.executionResult
+                            .addEvent((int) (System.currentTimeMillis() - _cmd.commandStartTimestamp), HystrixEventType.CANCELLED);
+                }
+            }
             @Override
             public void call() {
                 circuitBreaker.markNonSuccess();
                 if (_cmd.commandState.compareAndSet(CommandState.OBSERVABLE_CHAIN_CREATED, CommandState.UNSUBSCRIBED)) {
-                    if (!_cmd.executionResult.containsTerminalEvent()) {
-                        _cmd.eventNotifier.markEvent(HystrixEventType.CANCELLED, _cmd.commandKey);
-                        try {
-                            executionHook.onUnsubscribe(_cmd);
-                        } catch (Throwable hookEx) {
-                            logger.warn("Error calling HystrixCommandExecutionHook.onUnsubscribe", hookEx);
-                        }
-                        _cmd.executionResultAtTimeOfCancellation = _cmd.executionResult
-                                .addEvent((int) (System.currentTimeMillis() - _cmd.commandStartTimestamp), HystrixEventType.CANCELLED);
-                    }
+                    markTerminalEventData();
                     handleCommandEnd(false); //user code never ran
                 } else if (_cmd.commandState.compareAndSet(CommandState.USER_CODE_EXECUTED, CommandState.UNSUBSCRIBED)) {
-                    if (!_cmd.executionResult.containsTerminalEvent()) {
-                        _cmd.eventNotifier.markEvent(HystrixEventType.CANCELLED, _cmd.commandKey);
-                        try {
-                            executionHook.onUnsubscribe(_cmd);
-                        } catch (Throwable hookEx) {
-                            logger.warn("Error calling HystrixCommandExecutionHook.onUnsubscribe", hookEx);
-                        }
-                        _cmd.executionResultAtTimeOfCancellation = _cmd.executionResult
-                                .addEvent((int) (System.currentTimeMillis() - _cmd.commandStartTimestamp), HystrixEventType.CANCELLED);
-                    }
+                    markTerminalEventData();
                     handleCommandEnd(true); //user code did run
                 }
             }
--- hystrix-core/src/main/java/com/netflix/hystrix/HystrixThreadPool.java
+++ hystrix-core/src/main/java/com/netflix/hystrix/HystrixThreadPool.java
@@ -110,12 +110,24 @@ public interface HystrixThreadPool {
             }
 
             // if we get here this is the first time so we need to initialize
-            synchronized (HystrixThreadPool.class) {
+           /* synchronized (HystrixThreadPool.class) {
                 if (!threadPools.containsKey(key)) {
                     threadPools.put(key, new HystrixThreadPoolDefault(threadPoolKey, propertiesBuilder));
                 }
             }
-            return threadPools.get(key);
+            return threadPools.get(key);*/
+
+            HystrixThreadPool  hystrixThreadPool = threadPools.putIfAbsent(key, new HystrixThreadPoolDefault(threadPoolKey, propertiesBuilder));
+
+            if(hystrixThreadPool==null){
+
+                return threadPools.get(key);
+            }else{
+
+                return hystrixThreadPool;
+            }
+
+
         }
 
         /**
